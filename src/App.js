3e9505f239050beff49ece126b27ab33ba268e06
“AndrewPalet” <palet5796@gmail.com>
2020-12-10 16:11:55 -0600

    netlify deployment 2

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Test1
+          Test3 Continuos Deployment
         </a>
       </header>
     </div>
